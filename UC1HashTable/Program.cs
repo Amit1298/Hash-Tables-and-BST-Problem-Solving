d0a9bc1b89747a4602a6a0f6654fe6b4fe0dc0dc
Amit <amitkr24031995@gmail.com>
2021-10-07 18:13:41 +0530

    Adding Hash Table

---
+++ UC1HashTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UC1HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "To");
+            hash.Add("5", "be");
+            string hash1 = hash.Get("4");
+            Console.WriteLine(hash1);
+        }
+    }
+}
