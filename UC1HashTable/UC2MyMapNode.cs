6567c5bca046e350c92847e0306ea7255d5159da
Amit <amitkr24031995@gmail.com>
2021-10-08 18:51:21 +0530

    Added Para hash table

---
+++ UC1HashTable/UC2MyMapNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2paraHashTable
+{
+    class UC2MyMapNode<K, V>
+    {
+        private int size;
+        private LinkedList<KeyValue<K, V>>[] items;
+        public UC2MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedlist.AddLast(item);
+        }
+        public struct KeyValue<K, V>
+        {
+            public K Key { get; set; }
+            public V Value { get; set; }
+        }
+    }
+}
---
+++ UC1HashTable/UC2Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UC2paraHashTable
+{
+    class UC2Program
+    {
+        static void Main(string[] args)
+        {
+            UC2MyMapNode<string, string> hash = new UC2MyMapNode<string, string>(18);
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "deliberately");
+            hash.Add("15", "into");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
+            string hash1 = hash.Get("11");
+            Console.WriteLine(hash1);
+        }
+    }
+}
